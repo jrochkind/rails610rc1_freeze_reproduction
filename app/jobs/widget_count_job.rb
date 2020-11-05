7519d18a0c90fe8cc505348b0a8fe72c9d7081c7
Jonathan Rochkind <jrochkind@sciencehistory.org>
2020-11-05 14:44:57 -0500

    output about to start Widget.count as a marker

--- app/jobs/widget_count_job.rb
+++ app/jobs/widget_count_job.rb
@@ -2,6 +2,7 @@ class WidgetCountJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
+    puts "about to call Widget.count"
     $widget_count = Widget.count
     puts "Widget count: #{$widget_count}"
   end
